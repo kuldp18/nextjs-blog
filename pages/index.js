21e4cb333d41f65ab06f5024564a525f5291708e
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-05-01 14:28:11 +0530

    recent blogs kinda works on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import CustomHead from '../components/CustomHead';
+import BlogItem from '../components/BlogItem';
 
-export default function Home() {
+export default function Home(props) {
+  const [recentBlogs, setRecentBlogs] = useState(props.blogs.slice(0, 3));
   return (
     <div className={styles.container}>
       <CustomHead title="Hunting Coder" />
@@ -14,38 +17,29 @@ export default function Home() {
 
         <div className={styles.blogs}>
           <h2 style={{ fontSize: '2rem' }}>Latest Blogs</h2>
-          <div className="blog-item">
-            <h3>How to learn JavaScript in 2022?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              ipsa, adipisci repellendus tenetur, non harum nam voluptates
-              pariatur, necessitatibus sit sunt ullam ducimus ut vitae quia
-              incidunt aperiam quo. Sed maxime eligendi harum quis adipisci
-              dolor autem aliquid delectus omnis.
-            </p>
-          </div>
-          <div className="blog-item">
-            <h3>How to learn JavaScript in 2022?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              ipsa, adipisci repellendus tenetur, non harum nam voluptates
-              pariatur, necessitatibus sit sunt ullam ducimus ut vitae quia
-              incidunt aperiam quo. Sed maxime eligendi harum quis adipisci
-              dolor autem aliquid delectus omnis.
-            </p>
-          </div>
-          <div className="blog-item">
-            <h3>How to learn JavaScript in 2022?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              ipsa, adipisci repellendus tenetur, non harum nam voluptates
-              pariatur, necessitatibus sit sunt ullam ducimus ut vitae quia
-              incidunt aperiam quo. Sed maxime eligendi harum quis adipisci
-              dolor autem aliquid delectus omnis.
-            </p>
-          </div>
+          {/* rendering all blogs */}
+          {recentBlogs.map((blog) => {
+            const { title, slug, description } = blog;
+            return (
+              <BlogItem
+                title={title}
+                slug={slug}
+                description={description}
+                key={slug}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const response = await fetch('http://localhost:3000/api/blogs');
+  const blogs = await response.json();
+
+  return {
+    props: { blogs }, // will be passed to the page component as props
+  };
 }
