62569ad499793d233e9882b03799dea8ccf5b726
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-05-01 12:35:02 +0530

    .gitkeep file messes the index, so edited it

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     let data = await fs.promises.readdir('contactdata');
     // make new files incrementally for every response
     fs.promises.writeFile(
-      `contactdata/${data.length + 1}.json`,
+      `contactdata/${data.length}.json`,
       JSON.stringify(req.body)
     );
     res.status(200).json(req.body);
